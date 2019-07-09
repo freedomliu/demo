3d67f7e34f3dfe9190d4f3e0c8ed876eac9efd85
freedomliu <13478666105@162.com>
2019-07-09 15:57:43 +0800

    1

--- demo/src/main/java/com/example/demo/util/JSONResult.java
+++ demo/src/main/java/com/example/demo/util/JSONResult.java
@@ -3,14 +3,6 @@ package com.example.demo.util;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-/**
- * @Description: 自定义响应数据结构
- * 				200：表示成功
- * 				500：表示错误，错误信息在msg字段中
- * 				501：bean验证错误，不管多少个错误都以map形式返回
- * 				502：拦截器拦截到用户token出错
- * 				555：异常抛出信息
- */
 public class JSONResult {
 
     // 响应业务状态
@@ -25,6 +17,22 @@ public class JSONResult {
     // 附加数据
     private Object attach;
 
+    public static JSONResult ok() {
+        return new JSONResult(null);
+    }
+    
+    public static JSONResult ok(Object data) {
+        return new JSONResult(data);
+    }
+    
+    public static JSONResult ok(String msg) {
+        return new JSONResult(null,msg);
+    }
+    
+    public static JSONResult ok(Object data,String msg) {
+        return new JSONResult(data,msg);
+    }
+    
     public static JSONResult ok(Object data,String msg,Object attach) {
         return new JSONResult(data,msg,attach);
     }
@@ -32,51 +40,9 @@ public class JSONResult {
     public static JSONResult build(Integer status, String msg, Object data) {
         return new JSONResult(status, msg, data);
     }
-
-    public static JSONResult ok(Object data,String msg) {
-        return new JSONResult(data,msg);
-    }
     
-    public static JSONResult ok(Object data,Object attach) {
-    	return new JSONResult(data,attach);
-    }
-    
-    public static JSONResult ok(Object data) {
-        return new JSONResult(data);
-    }
-
-    public static JSONResult ok(String msg) {
-        return new JSONResult(null,msg);
-    }
-    
-    public static JSONResult ok() {
-        return new JSONResult(null);
-    }
-    
-    public static JSONResult errorMsg(String msg) {
+    public static JSONResult error(String msg) {
         return new JSONResult(500, msg, null);
-    }
-    
-    public static JSONResult errorMap(Object data) {
-        return new JSONResult(501, "error", data);
-    }
-    
-    public static JSONResult errorTokenMsg(String msg) {
-        return new JSONResult(502, msg, null);
-    }
-    
-    public static JSONResult errorException(String msg) {
-        return new JSONResult(555, msg, null);
-    }
-
-    public JSONResult() {
-
-    }
-
-    public JSONResult(Integer status, String msg, Object data) {
-        this.status = status;
-        this.msg = msg;
-        this.data = data;
     }
 
     public JSONResult(Object data) {
@@ -97,6 +63,7 @@ public class JSONResult {
     	this.data = data;
     	this.attach=attach;
     }
+    
     public JSONResult(Object data, String msg, Object attach) {
     	this.status = 200;
     	this.msg = "OK";
@@ -105,42 +72,12 @@ public class JSONResult {
     	this.attach=attach;
     }
 
-    public Boolean isOK() {
-        return this.status == 200;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
+    public JSONResult(Integer status, String msg, Object data) {
         this.status = status;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
         this.msg = msg;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
         this.data = data;
     }
-
-	public Object getAttach() {
-		return attach;
-	}
-
-	public void setAttach(Object attach) {
-		this.attach = attach;
-	}
-
+    
 	/**
 	 * @Decription: 获取异常信息
 	 * @Author: liuxiangtao90
@@ -157,4 +94,38 @@ public class JSONResult {
         sw.flush();
         return sw.toString();
     }
+	
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public Object getAttach() {
+		return attach;
+	}
+
+	public void setAttach(Object attach) {
+		this.attach = attach;
+	}
+
+
 }
